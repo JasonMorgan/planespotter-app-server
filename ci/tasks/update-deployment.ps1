0f2a7e994ae0b80ff62573d9f13f338062ffebf2
Jason Morgan <jmorgan@f9vs.com>
2018-05-14 22:50:11 -0600

    fixing a path.

--- ci/tasks/update-deployment.ps1
+++ ci/tasks/update-deployment.ps1
@@ -1,6 +1,6 @@
 $file = Get-ChildItem -Path cluster-repo/Deployment/$ENV:DEPLOYMENT
 $file
-$newName = "$($ENV:IMAGE_NAME):$(Get-Content -Raw -Path source/metadata/tag)"
+$newName = "$($ENV:IMAGE_NAME):$(Get-Content -Raw -Path source/ci/metadata/tag)"
 $newName
 $string = (Get-Content -Raw -Path $file.FullName) -replace "image: .*", "image: $newName"
 $string
