2edb9876552c2b38ae43296d7c6e0975b97887da
Jason Morgan <jmorgan@f9vs.com>
2018-05-15 07:07:12 -0600

    missing init.

--- ci/tasks/update-deployment.ps1
+++ ci/tasks/update-deployment.ps1
@@ -2,7 +2,9 @@ $file = Get-ChildItem -Path cluster-repo/Deployment/$ENV:DEPLOYMENT
 $newName = "$($ENV:IMAGE_NAME):$(Get-Content -Raw -Path source/ci/metadata/tag)"
 $string = (Get-Content -Raw -Path $file.FullName) -replace "image: .*", "image: $newName"
 $string | Set-Content $file.FullName
+Get-ChildItem bump
 Copy-Item -Recurse -Path cluster-repo/* -Destination out/
 cd out
+git init
 git add .
 git commit -m "Updated via Concourse."
