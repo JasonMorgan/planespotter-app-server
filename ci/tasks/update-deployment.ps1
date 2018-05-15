7dea6c172e98029ae4f2902a9d8d7b7eaafbc309
Jason Morgan <jmorgan@f9vs.com>
2018-05-15 07:27:42 -0600

    Adding git attributes.

--- ci/tasks/update-deployment.ps1
+++ ci/tasks/update-deployment.ps1
@@ -2,13 +2,13 @@ $file = Get-ChildItem -Path cluster-repo/Deployment/$ENV:DEPLOYMENT
 $newName = "$($ENV:IMAGE_NAME):$(Get-Content -Raw -Path source/ci/metadata/tag)"
 $string = (Get-Content -Raw -Path $file.FullName) -replace "image: .*", "image: $newName"
 $string | Set-Content $file.FullName
-Get-ChildItem bump -Recurse
+Get-ChildItem bump -Recurse | foreach { $_ | get-content}
 # cat bump/ci/metadata/tag
 Get-ChildItem -path cluster-repo -force
 Copy-Item -Recurse -Path cluster-repo/* -Destination out/ -Force
 cd out
 # git init
-# git config --global user.email "jmorgan@f9vs.com"
-# git config --global user.name "JasonMorgan"
+git config --global user.email "jmorgan@f9vs.com"
+git config --global user.name "JasonMorgan"
 git add .
 git commit -m "Updated via Concourse."
