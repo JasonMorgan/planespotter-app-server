af9b5e6d544576456342122fb44b270ded3016f8
Jason Morgan <jmorgan@f9vs.com>
2018-05-14 22:39:58 -0600

    Arbitrary changes to figure out wtf is happening.

--- ci/tasks/update-deployment.ps1
+++ ci/tasks/update-deployment.ps1
@@ -3,3 +3,4 @@ $newName = "$($ENV:IMAGE_NAME):$(Get-Content -Raw -Path source/metadata/tag)"
 $string = (Get-Content -Raw -Path $file.FullName) -replace "image: .*", "image: $newName"
 $string | Set-Content $file.FullName
 Copy-Item -Recurse -Path source/* -Destination out/
+cat out/Deployment/$ENV:DEPLOYMENT
