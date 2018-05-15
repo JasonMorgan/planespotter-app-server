537b0a0f3ce042bb2d784b3a93994e8126db9b06
Jason Morgan <jmorgan@f9vs.com>
2018-05-14 22:47:16 -0600

    Adding some outputs.

--- ci/tasks/update-deployment.ps1
+++ ci/tasks/update-deployment.ps1
@@ -1,6 +1,9 @@
 $file = Get-ChildItem -Path cluster-repo/Deployment/$ENV:DEPLOYMENT
+$file
 $newName = "$($ENV:IMAGE_NAME):$(Get-Content -Raw -Path source/metadata/tag)"
+$newName
 $string = (Get-Content -Raw -Path $file.FullName) -replace "image: .*", "image: $newName"
+$string
 $string | Set-Content $file.FullName
 Copy-Item -Recurse -Path source/* -Destination out/
 cat out/Deployment/$ENV:DEPLOYMENT
