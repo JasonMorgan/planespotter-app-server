c3c3283f094eed8de0b06b913014d7adcd176d5e
Jason Morgan <jmorgan@f9vs.com>
2018-05-15 07:21:04 -0600

    Trying again.

--- ci/tasks/update-deployment.ps1
+++ ci/tasks/update-deployment.ps1
@@ -2,12 +2,13 @@ $file = Get-ChildItem -Path cluster-repo/Deployment/$ENV:DEPLOYMENT
 $newName = "$($ENV:IMAGE_NAME):$(Get-Content -Raw -Path source/ci/metadata/tag)"
 $string = (Get-Content -Raw -Path $file.FullName) -replace "image: .*", "image: $newName"
 $string | Set-Content $file.FullName
-Get-ChildItem bump
-cat bump/ci/metadata/tag
-Copy-Item -Recurse -Path cluster-repo/* -Destination out/
+Get-ChildItem bump -Recurse
+# cat bump/ci/metadata/tag
+Get-ChildItem -path cluster-repo -force
+Copy-Item -Recurse -Path cluster-repo/* -Destination out/ -Force
 cd out
-git init
-git config --global user.email "jmorgan@f9vs.com"
-git config --global user.name "JasonMorgan"
+# git init
+# git config --global user.email "jmorgan@f9vs.com"
+# git config --global user.name "JasonMorgan"
 git add .
 git commit -m "Updated via Concourse."
