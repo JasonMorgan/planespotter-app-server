22ffd7d434445ced18663155fb1b7c3f3b6b5c8f
Jason Morgan <jmorgan@f9vs.com>
2018-05-15 00:11:34 -0600

    dropping prints.

--- ci/tasks/update-deployment.ps1
+++ ci/tasks/update-deployment.ps1
@@ -1,13 +1,8 @@
 $file = Get-ChildItem -Path cluster-repo/Deployment/$ENV:DEPLOYMENT
-$file
 $newName = "$($ENV:IMAGE_NAME):$(Get-Content -Raw -Path bump/ci/metadata/tag)"
-$newName
 $string = (Get-Content -Raw -Path $file.FullName) -replace "image: .*", "image: $newName"
-$string
 $string | Set-Content $file.FullName
 Copy-Item -Recurse -Path cluster-repo/* -Destination out/
 cd out
 git add .
 git commit -m "Updated via Concourse."
-Get-ChildItem out
-cat out/Deployment/$ENV:DEPLOYMENT
